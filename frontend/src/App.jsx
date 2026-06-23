b50f5e8d5da0b9354269e46aaa03c4302a51f21d
Ashutosh Kumar <ashutoshsrivasstava0000@gmail.com>
2026-06-23 16:20:42 +0530

    Frontend deployment config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:8000"
+const API_URL = import.meta.env.VITE_API_URL;
 
 export default function App() {
   const [products, setProducts] = useState([])
